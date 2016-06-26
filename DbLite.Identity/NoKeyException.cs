0ad063ddb422c54a16dcd30fe23ebe3c15a11186
Nick Stenroos-Dam <nisd123@gmail.com>
2016-06-26 16:43:41 +0200

    Work in progress user store for Asp.net Identity Core

---
+++ DbLite.Identity/NoKeyException.cs
@@ -0,0 +1,20 @@
+﻿namespace DbLite.Identity
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [Serializable]
+    public class NoKeyException : Exception
+    {
+        public NoKeyException() { }
+        public NoKeyException(string message) : base(message) { }
+        public NoKeyException(string message, Exception inner) : base(message, inner) { }
+        protected NoKeyException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context)
+        { }
+    }
+}
